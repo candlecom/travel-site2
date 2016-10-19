ce7f1ab7638844829a9e5bb67eacdeb2a417b42c
Bruce <valpy@nnsl.com>
2016-10-18 19:37:27 -0600

    Updated package_json

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch');
+
+gulp.task('default', function() {
+  console.log("Hooray")
+});
+
+gulp.task('html', function() {
+  console.log("imagine");
+});
+
+gulp.task('styles', function() {
+  console.log("Imagine SASS");
+})
+
+gulp.task('watch', function() {
+
+  watch('./app/index.html', function() {
+    gulp.start('html');
+
+  });
+
+  watch('./app/assets/styles/**/*', function() {
+    gulp.start('styles');
+  });
+
+});
