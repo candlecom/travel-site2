fd4461ff166a0cd6b6438f13f9e9f455de84e743
Bruce <valpy@nnsl.com>
2016-11-14 17:30:20 -0700

    Mobile header files

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,10 @@
+function person(name, favColor) {
+  console.log("Hello, my name is " + name + " and my favourite color is " + favColor + ".");
+}
+
+var john = {
+  name: "John Doe",
+  favouriteColor: "blue"
+}
+person(john.name, john.favoriteColor);
+person("Jane Smith", "green");
