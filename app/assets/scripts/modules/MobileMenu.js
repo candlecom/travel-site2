e68b483f2b29b84f1cb74e4b2f0457b4690bf9a1
Bruce <valpy@nnsl.com>
2016-11-18 16:06:36 -0700

    Merging header files

---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+
+class MobileMenu {
+  constructor() {
+    this.menuIcon = $(".site-header_menu-icon");
+    this.events();
+  }
+
+
+events() {
+  this.menuIcon.click(this.toggleTheMenu);
+}
+
+toggleTheMenu() {
+  console.log("Hooray - the icon was clicked.");
+  }
+}
+
+export default MobileMenu;
