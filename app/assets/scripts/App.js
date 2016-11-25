93a23c7fb9bd87ff1e72f2e2d9bb82b2415a004a
Bruce <valpy@nnsl.com>
2016-11-24 17:12:10 -0700

    Modal files

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,16 +1,13 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
-<<<<<<< HEAD
-=======
 import StickyHeader from './modules/StickyHeader';
->>>>>>> sticky-header
+import Modal from './modules/Modal';
+
+
 
 var mobileMenu = new MobileMenu();
-
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
-<<<<<<< HEAD
-=======
 var stickyHeader = new StickyHeader();
->>>>>>> sticky-header
+var modal = new Modal();
